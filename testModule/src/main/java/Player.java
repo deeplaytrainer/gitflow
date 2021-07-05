6f75c3d28c94808bd20c02ad20d3c4d7c4359fb2
Degree <degree@deeplay.io>
2021-07-05 11:34:37 +0600

    [+] create Player.java

---
+++ testModule/src/main/java/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    private final String name;
+    private final String color;
+
+    public Player(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+}
